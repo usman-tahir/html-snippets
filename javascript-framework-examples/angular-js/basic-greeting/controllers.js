ebf8d760964ff83b72c38076fde0829e15c6444b
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-11-04 19:16:00 -0400

    added a simple greeting/date application with an ng-click button

---
+++ javascript-framework-examples/angular-js/basic-greeting/controllers.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+  
+  angular.module("myApp.controllers", []).controller("SimpleDate", function($scope) {
+    $scope.helloMessages = ["Hello", "Bonjour", "Hola", "Ciao", "Hallo"];
+    $scope.greeting = $scope.helloMessages[0];
+    $scope.date = new Date();
+    $scope.getRandomHelloMessage = function () {
+      $scope.greeting = $scope.helloMessages[parseInt((Math.random() * $scope.helloMessages.length))];
+    }
+  });
+  
+}());
