803afd81693276d428798401bd45cc22820a6544
usman-tahir <tahir.usman.ali94@outlook.com>
2016-11-04 11:08:38 -0400

    added an app.js and controller.js file for the salary app

---
+++ javascript-framework-examples/angular-js/salary/controllers.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+  
+  angular.module("myApp.controllers", []).controller("FinanceController", function($scope) {
+    $scope.salary = 0;
+    $scope.percentage = 0;
+    $scope.result = function() {
+      return $scope.salary * $scope.percentage * 0.01;
+    };
+  });
+  
+}());
