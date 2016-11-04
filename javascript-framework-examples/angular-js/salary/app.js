225439a82382b5966724622975c9f62904968dcb
usman-tahir <tahir.usman.ali94@outlook.com>
2016-11-04 15:31:14 -0400

    added the currency filter for output; added comments for clarity

--- javascript-framework-examples/angular-js/salary/app.js
+++ javascript-framework-examples/angular-js/salary/app.js
@@ -1,7 +1,9 @@
 (function () {
   "use strict";
   
+  // Defines the controller for myApp
   angular.module("myApp", [
     "myApp.controllers"
   ]);
+  
 }());
--- javascript-framework-examples/angular-js/salary/controllers.js
+++ javascript-framework-examples/angular-js/salary/controllers.js
@@ -1,9 +1,12 @@
 (function () {
   "use strict";
   
+  // The myApp Controller
   angular.module("myApp.controllers", []).controller("FinanceController", function($scope) {
-    $scope.salary = 0;
-    $scope.percentage = 0;
+    $scope.salary = 0; // Set the salary value to 0
+    $scope.percentage = 0; // Set the percentage value to 0
+    
+    // Calculate the result
     $scope.result = function() {
       return $scope.salary * $scope.percentage * 0.01;
     };
