6bd734da491e58092418e789ea6281707eb2917a
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-23 20:49:45 -0500

    refactored the quiz ninja code

--- js-novice-to-ninja/quiz-ninja-project/scripts.js
+++ js-novice-to-ninja/quiz-ninja-project/scripts.js
@@ -6,21 +6,39 @@ var quiz = [
   ["What is Superman's real name?", "Clark Kent"],
   ["What is Wonderwoman's real name?", "Diana Prince"],
   ["What is Batman's real name?", "Bruce Wayne"]
-],
+  ],
   score = 0,
   max = quiz.length,
   i;
 
-for (i = 0; i < max; i += 1) {
-  var answer = prompt(quiz[i][0]);
+play(quiz);
+
+function play(quiz) {
+  // Main game loop
+  var i,
+    max = quiz.length;
   
-  // Check if the answer is correct
-  if (answer === quiz[i][1]) {
-    alert("Correct!");
-    score += 1;
-  } else {
-    alert("Wrong.");
+  for (i = 0; i < max; i += 1) {
+    question = quiz[i][0];
+    answer = ask(question);
+    check(answer);
+  }
+  gameOver();
+  
+  function ask(question) {
+    return prompt(question);
+  }
+
+  function check(answer) {
+    if (answer === quiz[i][1]) {
+      alert("Correct!");
+      score += 1;
+    } else {
+      alert("Incorrect.");
+    }
+  }
+
+  function gameOver() {
+    alert("You scored a " + score + " out of " + max + "!");
   }
 }
-
-alert("You scored a " + score + " out of " + max);
