a6445804346beacad826cf7878a5dbe3bb52062c
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-23 21:55:26 -0500

    added some Math object examples

---
+++ js-novice-to-ninja/math/scripts.js
@@ -0,0 +1,79 @@
+
+// Examples with the Math object
+
+console.log(Math.PI);
+
+console.log(Math.SQRT2); // The square root of 2
+
+console.log(Math.SQRT1_2); // The reciprocal of the square root of 2
+
+console.log(Math.E); // Euler's constant
+
+console.log(Math.LN2); // The natural logarithm of 2
+
+console.log(Math.LN10); // The natural logarithm of 10
+
+console.log(Math.LOG2E); // Log base 2 of Euler's constant
+
+console.log(Math.LOG10E); // Log base 10 of Euler's constant
+
+console.log(Math.abs(3));
+
+console.log(Math.abs(-4.5));
+
+console.log(Math.ceil(4.3));
+
+console.log(Math.ceil(-4.3));
+
+console.log(Math.floor(5.2));
+
+console.log(Math.floor(-5.2));
+
+console.log(Math.round(4.5));
+
+console.log(Math.round(4.499));
+
+
+// Raises a number to the power of Euler's constant
+
+console.log(Math.exp(1)); // Euler's constant
+
+console.log(Math.exp(0)); // Any number raised to the power of 0 is 1
+
+console.log(Math.exp(-3));
+
+console.log(Math.pow(3, 2)); // 3 raised to the power of 2
+
+console.log(Math.pow(27, 1 / 3)); // Cube root of 27 (3)
+
+console.log(Math.sqrt(121));
+
+console.log(Math.sqrt(-1)); // No support for imaginary numbers
+
+console.log(Math.log(Math.E)); // Natural logarithms have a base of Euler's constant
+
+console.log(Math.log(1)); // Log of 1 is 0
+
+console.log(Math.log(0)); // You cannot take the log of 0
+
+console.log(Math.log(-2)); // You cannot take the log of negative numbers
+
+console.log(Math.max(1, 2, 3));
+
+console.log(Math.max(Math.PI, Math.SQRT2, Math.E));
+
+console.log(Math.min(1, 2, 3));
+
+console.log(Math.min(Math.PI, Math.SQRT2, Math.E));
+
+console.log(Math.sin(Math.PI / 6)); // sin(pi / 6)
+
+console.log(Math.cos(Math.PI / 4)); // cos(pi / 4)
+
+console.log(Math.tan(Math.PI / 4)); // tan(pi / 4)
+
+console.log(Math.random());
+
+console.log(6 * Math.random());
+
+console.log(Math.floor(6 * Math.random())); // An integer between 0 and 5 inclusive
