55aa18a595228bbfc51a90080170f8ab410f891a
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-27 08:57:39 -0500

    added document writing examples and cookie/interval functionality examples

--- js-novice-to-ninja/bom/scripts.js
+++ js-novice-to-ninja/bom/scripts.js
@@ -17,7 +17,7 @@ console.log(window.location.hash || "none");
 console.log(window.location.origin || "none");
 
 // Opening up a custom pop-up
-var popup = window.open("https://sitepoint.com", "Sitepoint", "width=400, height=400, resizable=yes");
+// var popup = window.open("https://sitepoint.com", "Sitepoint", "width=400, height=400, resizable=yes");
 // popup.close();
 // popup.moveTo(100, 100);
 // popup.resizeTo(600, 400);
@@ -26,3 +26,28 @@ console.log("Current width/height of screen --> width: " + window.screen.width +
 console.log("Available width/height for screen --> width: " + window.screen.availWidth + ", height: " + window.screen.availHeight);
 
 console.log(window.screen.colorDepth);
+
+/*
+  DOM writing
+*/
+document.write("Hello World!");
+
+/*
+  Cookies
+*/
+document.cookie = "name=Superman";
+console.log(document.cookie);
+
+// Expiration date for cookies
+var expirationDate = new Date(),
+  tomorrow = expirationDate.getTime() + (1000 * 60 * 60 * 24);
+expirationDate.setTime(tomorrow);
+document.cookie = "expires=" + expirationDate.toUTCString();
+
+// Timing functions
+function log() {
+  console.log("Logging to the console");
+}
+
+// var repeat = window.setInterval(log, 1000);
+// window.clearInterval(repeat);
