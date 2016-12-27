7514351e5fb5e5e93f8392854bfd5c92a8d7ea19
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-26 22:35:19 -0500

    added form functionality to the quiz ninja project; cleaned up the UI

--- js-novice-to-ninja/quiz-ninja-project/scripts.js
+++ js-novice-to-ninja/quiz-ninja-project/scripts.js
@@ -25,6 +25,9 @@ var $scoreContainer = document.getElementById("score");
 var $feedbackContainer = document.getElementById("feedback");
 var $start = document.getElementById("start");
 
+// Form submission for the answer
+var $form = document.getElementById("answer");
+
 function update(element, content, appliedClass) {
   var p = element.firstChild || document.createElement("p");
   console.log(p);
@@ -35,27 +38,47 @@ function update(element, content, appliedClass) {
   }
 }
 
+function hide (element) {
+  element.style.display = "none";
+}
+
+function show (element) {
+  element.style.display = "block";
+}
+
+
 // Quiz start button
 $start.addEventListener("click", function () {
   play(quiz);
 }, false);
+
+hide($form);
 
 function play(quiz) {
   
   var score = 0;
   update($scoreContainer, score);
   
-  // Main game loop
-  for (var i = 0, question, answer, max = quiz.questions.length; i < max; i += 1) {
-    question = quiz.questions[i].question;
-    answer = ask(question);
-    check(answer);
+  hide($start);
+  show($form);
+  
+  $form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    check($form[0].value);
+  }, false);
+  
+  var i = 0;
+  chooseQuestion();
+  
+  function chooseQuestion() {
+    var question = quiz.questions[i].question;
+    ask(question);
   }
-  gameOver();
   
   function ask(question) {
     update($questionContainer, quiz.question + question);
-    return prompt("Enter your answer:");
+    $form[0].value = "";
+    $form[0].focus;
   }
 
   function check(answer) {
@@ -66,10 +89,20 @@ function play(quiz) {
     } else {
       update($feedbackContainer, "Incorrect!", "incorrect");
     }
+    
+    i += 1;
+    
+    if (i === quiz.questions.length) {
+      gameOver();
+    } else {
+      chooseQuestion();
+    }
   }
 
   function gameOver() {
     // alert("You scored a " + score + " out of " + max + "!");
     update($questionContainer, "Game Over, you scored " + score + " points.");
+    hide($form);
+    show($start);
   }
 }
