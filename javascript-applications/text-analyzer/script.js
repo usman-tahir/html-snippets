aaaab05432a531814f6af540d13173fe3c425ce2
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-01-17 21:02:45 -0500

    added a space counting method; fixed a minor bug when counting the number of words

--- javascript-applications/text-analyzer/script.js
+++ javascript-applications/text-analyzer/script.js
@@ -7,34 +7,55 @@
     analysisOutput = document.getElementById("analysisOutput");
 
   function getNumberOfWords(input) {
-    console.log(input);
-    var words = input.split(" ");
-    console.log(words);
-    return "Number of words: " + words.length;
+    var words = input.split(" "),
+      pattern = /[a-zA-Z]/,
+      count = 0,
+      i;
+
+    for (i = 0; i < words.length; i += 1) {
+      if (words[i].match(pattern)) {
+        count += 1;
+      }
+    }
+
+    return "Number of words: " + count;
   }
 
   function getNumberOfLetters(input) {
-    var words = input.split(""),
+    var letters = input.split(""),
       pattern = /[a-zA-Z]/, // Uppercase and lowercase letters
       count = 0,
       i;
-    for (i = 0; i < words.length; i += 1) {
-      if (words[i].match(pattern)) {
+    for (i = 0; i < letters.length; i += 1) {
+      if (letters[i].match(pattern)) {
         count += 1;
       }
     }
     return "Number of letters: " + count;
   }
 
+  function getNumberOfSpaces(input) {
+    var characters = input.split(""),
+      pattern = /[\s\w+]/,
+      count = 0,
+      i;
+
+    for (i = 0; i < characters.length; i += 1) {
+      if (characters[i].match(pattern)) {
+        count += 1;
+      }
+    }
+    return "Number of spaces: " + count;
+  }
+
   function analysis() {
     var output = "",
       text = textToAnalyze.value,
       numberOfWords = getNumberOfWords(text),
-      numberOfLetters = getNumberOfLetters(text);
-    /*
-      TODO: All other points of analysis
-    */
-    output += numberOfWords + "<br />" + numberOfLetters;
+      numberOfLetters = getNumberOfLetters(text),
+      numberOfSpaces = getNumberOfSpaces(text);
+
+    output += numberOfWords + "<br />" + numberOfLetters + "<br />" + numberOfSpaces;
     analysisOutput.innerHTML = output;
   }
   analyzeButton.addEventListener("click", analysis, false);
