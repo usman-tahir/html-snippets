3cfef76aa4ea059b4a11c6bb2c6ad66632922362
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-02-03 20:16:03 -0500

    added a basic template creation/rendering example

--- javascript-framework-examples/backbone-marionette-app/contact-app/assets/js/app.js
+++ javascript-framework-examples/backbone-marionette-app/contact-app/assets/js/app.js
@@ -2,8 +2,15 @@
   'use strict';
   var ContactManager = new Marionette.Application();
 
+  ContactManager.StaticView = Marionette.ItemView.extend({
+    el: '#main-region',
+    template: '#static-template'
+  });
+
   ContactManager.on('start', function () {
     console.log('Contact Manager has started.');
+    var staticView = new ContactManager.StaticView();
+    staticView.render();
   });
 
   ContactManager.start();
