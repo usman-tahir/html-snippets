fd59a3eb04775155d23ac53c57c5ac75444a8c58
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-02-08 20:22:28 -0500

    updated rendering and view/collection definitions

--- javascript-framework-examples/backbone-js/surfboards/app.js
+++ javascript-framework-examples/backbone-js/surfboards/app.js
@@ -2,54 +2,91 @@
 
   'use strict';
 
+  // Surfboard model
   var Surfboard = Backbone.Model.extend({
     defaults: {
       manufacturer: '',
       model: '',
-      stock: 0,
-      unitPrice: 0.00
+      stock: 0
     }
   });
 
+  // New instance of Surfboard model - board1
+  var board1 = new Surfboard({
+    manufacturer: 'Channel Islands',
+    model: 'Whip',
+    stock: 12,
+    unitPrice: 115.00
+  });
+
+  // New instance of Surfboard model - board2
+  var board2 = new Surfboard({
+    manufacturer: 'Lost',
+    model: 'Sub Scorcher',
+    stock: 9,
+    unitPrice: 45.00
+  });
+
+  // New instance of Surfboard model - board3
+  var board3 = new Surfboard({
+    manufacturer: 'Firewire',
+    model: 'Spitfire',
+    stock: 5,
+    unitPrice: 55.00
+  });
+
+  // Create Surfboards Collection
   var SurfboardsCollection = Backbone.Collection.extend({
     model: Surfboard
   });
 
-  var boardOne = new Surfboard({
-    manufacturer: 'Channel Islands',
-    model: 'Whip',
-    stock: 12,
-    unitPrice: 40.00
-  }),
-    boardTwo = new Surfboard({
-      manufacturer: 'Riptide',
-      model: 'Cruiser',
-      stock: 10,
-      unitPrice: 55.00
-    }),
-    boardThree = new Surfboard({
-      manufacturer: 'Windy',
-      model: 'Sailor',
-      stock: 5,
-      unitPrice: 75.00
-    });
+  // Create new instance of SurfboardsCollection
+  // and add three model instances to it.
+  var Surfboards = new SurfboardsCollection;
+  Surfboards.add(board1);
+  Surfboards.add(board2);
+  Surfboards.add(board3);
 
-  // Add the surfboards
-  var Surfboards = new SurfboardsCollection();
-  Surfboards.add(boardOne);
-  Surfboards.add(boardTwo);
-  Surfboards.add(boardThree);
+  // Create Surfboards View
+  var SurfboardsView = Backbone.View.extend({
 
-  Surfboards.each(function (surfboard) {
-    $('#table-body').append(
-      '<tr>' +
-        '<td>' + surfboard.get('manufacturer') + '</td>' +
-        '<td>' + surfboard.get('model') + '</td>' +
-        '<td>' + surfboard.get('stock') + '</td>' +
-        '<td>$' + surfboard.get('unitPrice') + '</td>' +
-      '</td>'
-    );
+    el: '#table-body',
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      // this.$el.html('');
+
+      Surfboards.each(function(model) {
+        var surfboard = new SurfboardView({
+          model: model
+        });
+
+        this.$el.append(surfboard.render().el);
+      }.bind(this));
+
+      return this;
+    }
+
   });
 
+  // Create Surfboard View
+  var SurfboardView = Backbone.View.extend({
+
+    tagName: 'tr',
+
+    template: _.template($('#surfboard-template').html()),
+
+    render: function() {
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    }
+
+  });
+
+  // Launch app
+  var app = new SurfboardsView;
 
 }());
