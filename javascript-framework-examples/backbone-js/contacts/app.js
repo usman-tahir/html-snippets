e86728867c2f00022dd1c71f203af214759afcbb
usman-tahir <tahir.usman.ali94@outlook.com>
2017-02-20 10:26:11 -0500

    added a contact.js file for contact storage; created a table output for the ContactsDirectory collection

--- javascript-framework-examples/backbone-js/contacts/app.js
+++ javascript-framework-examples/backbone-js/contacts/app.js
@@ -1,7 +1,7 @@
-/*global Backbone, console*/
+/*global Backbone, console, $*/
 (function () {
   'use strict';
-  var ContactModel = Backbone.View.extend({
+  var ContactModel = Backbone.Model.extend({
     defaults: {
       firstName: '',
       lastName: '',
@@ -9,31 +9,24 @@
       occupation: ''
     }
   }),
-    contacts = [
-      {
-        firstname: 'John',
-        lastName: 'Doe',
-        age: 18,
-        occupation: 'Student'
-      },
-      {
-        firstname: 'Jane',
-        lastName: 'Doe',
-        age: 18,
-        occupation: 'Student'
-      },
-      {
-        firstName: 'John',
-        lastName: 'Public',
-        age: 40,
-        occupation: 'Farmer'
-      }
-    ],
-    ContactCollection = Backbone.Collection.extend({}),
+    ContactCollection = Backbone.Collection.extend({
+      model: ContactModel
+    }),
     ContactsDirectory = new ContactCollection();
   
-  contacts.forEach(function (contact) {
+  CONTACTS.forEach(function (contact) {
     console.log('added a new contact: ', JSON.stringify(contact));
     ContactsDirectory.add(new ContactModel(contact));
+  });
+  
+  ContactsDirectory.each(function (contactModel) {
+    $('#table-body').append(
+      '<tr>' +
+        '<td>' + contactModel.get('firstName') + '</td>' +
+        '<td>' + contactModel.get('lastName') + '</td>' +
+        '<td>' + contactModel.get('age') + '</td>' +
+        '<td>' + contactModel.get('occupation') + '</td>' +
+        '</tr>'
+    );
   });
 }());
---
+++ javascript-framework-examples/backbone-js/contacts/contacts.js
@@ -0,0 +1,26 @@
+var CONTACTS = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+    occupation: "Student"
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 18,
+    occupation: "Student"
+  },
+  {
+    firstName: "John",
+    lastName: "Public",
+    age: 40,
+    occupation: "Farmer"
+  },
+  {
+    firstName: "Jane",
+    lastName: "Public",
+    age: 38,
+    occupation: "Doctor"
+  }
+];
